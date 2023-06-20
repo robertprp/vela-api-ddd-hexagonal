85e278d55a9811e1deb46fd4c9a0db6adb2bf968
Roberto Pérez Rico <43745122+pretended@users.noreply.github.com>
2023-06-20 10:51:05 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,33 @@
 import mongoose from "mongoose";
 
+
+import mongoose from 'mongoose'
+import Chai from 'chai'
+import express from 'express'
+
+declare global {
+  // Globals
+  type Nullable<T> = T | null
+  type DatabaseSession = Nullable<mongoose.ClientSession>
+  type InferSchema<T> = mongoose.InferSchemaType<T> & {
+    _id: ObjectId
+    created: Date
+    updated: Date
+  }
+
+  // Express specific
+  type Request = express.Request
+  type Response = express.Response
+  type NextFunction = express.NextFunction
+  // TODO: This should extend with the user model when we have it in typescript
+  type AuthenticatedRequest = Request & {
+    user: {
+      _id: ObjectId
+    }
+  }
+
+  // Testing
+  const expect: Chai.ExpectStatic
+}
+
 export type ObjectId = string | mongoose.Types.ObjectId;
